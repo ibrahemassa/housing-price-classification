a88443e578c499c7d027c782c6ea0fe7be41ed25
ibrahemassa <oioi.ll58@outlook.com>
2026-01-06 10:44:18 +0300

    HW

--- integration_tests/test_prediction.py
+++ integration_tests/test_prediction.py
@@ -1,15 +1,19 @@
 import joblib
 import numpy as np
 from pathlib import Path
-
+from scipy.sparse import csr_matrix
 
 def test_model_can_make_prediction():
     processed_dir = Path("data/processed")
+    model_dir = Path("models")
 
     X_test, _ = joblib.load(processed_dir / "test.pkl")
-    model = joblib.load(processed_dir / "model.pkl")
+    model = joblib.load(model_dir / "model.pkl")
 
-    X_sample = X_test[0]
+    if not hasattr(X_test, "__getitem__"):
+        X_test = csr_matrix(X_test)
+
+    X_sample = X_test[0:1]
 
     prediction = model.predict(X_sample)
 
